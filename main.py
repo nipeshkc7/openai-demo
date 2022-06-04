b32639ebd11075aa0eceb1ab27f58239903b621b
Arpan KC <nipeshkc7@gmail.com>
2022-06-04 10:23:19 +1000

    update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+print('Hi, Im Marv the sarcastic robot \n')
+print('Ask me a question...')
+userInput = input()
+
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt=userInput,
+  temperature=0.5,
+  max_tokens=60,
+  top_p=0.3,
+  frequency_penalty=0.5,
+  presence_penalty=0
+)
+
+print(response.choices[0].text);
